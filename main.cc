b1595c20b91b9352d04fd76ef246882f6bdef7ba
Rizqi Ghozali <riz.ghz@gmail.com>
2023-01-11 13:30:46 +0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int graph[4][4] = {
-    {0, 30, 28, 41},    // A
-    {30, 0, 18, 15},    // B
-    {28, 18, 0, 21},    // C
-    {41, 15, 21, 0},    // D
+    {0, 30, 28, 41},  /* Rumah A */
+    {30, 0, 18, 15},  /* Rumah B */
+    {28, 18, 0, 21},  /* Rumah C */
+    {41, 15, 21, 0},  /* Rumah D */
 };
 
-std::vector<std::pair<int, int>> solusi;
+vector<pair<int, int>> Solusi;
 
-bool isVisited(int vertex) {
-    for (const std::pair<int, int> &i : solusi) {
+bool isVisited(const int& vertex) {
+    for (const pair<int, int>& i : Solusi) {
         if (vertex == i.first) {
             return true;
         }
@@ -19,38 +20,61 @@ bool isVisited(int vertex) {
     return false;
 }
 
-void FindShortest(int n, int start, int end, int& edge, int& vertex) {
+void ShortestRoute(int rumah, int start, int end, int& edge, int& vertex) {
+    /* deklarasi */
+    int mid;
+    static int e[2];
+    static int v[2];
+    int s[2];
+    /* algoritma */
     if (end - start == 1) {
-        int s[2] = {
-            graph[n][start],
-            graph[n][end],
-        };
+        s[0] = graph[rumah][start];
+        s[1] = graph[rumah][end];
         if (s[0] < s[1]) {
-            vertex = s[0] != 0 and s[0] != edge ? start : end;
-            edge = s[0] != 0 and s[0] != edge ? s[0] : s[1];
+            if (s[0] != 0 and s[0] != edge) {
+                vertex = start;
+                edge = s[0];
+            } else {
+                vertex = end;
+                edge = s[1];
+            }
         } else {
-            vertex = s[1] != 0 and s[1] != edge ? end : start;
-            edge = s[1] != 0 and s[1] != edge ? s[1] : s[0];
+            if (s[1] != 0 and s[1] != edge) {
+                vertex = end;
+                edge = s[1];
+            } else {
+                vertex = start;
+                edge = s[0];
+            }
         }
-        return;
-    }
-    int mid = (start + end) / 2;
-    static int e[2], v[2];
-    FindShortest(n, start, mid, e[0], v[0]);
-    FindShortest(n, mid + 1, end, e[1], v[1]);
-    if (e[0] < e[1]) {
-        edge = e[1] = not isVisited(v[0]) ? e[0] : e[1];
-        vertex = v[1] = not isVisited(v[0]) ? v[0] : v[1];
     } else {
-        edge = e[0] = not isVisited(v[1]) ? e[1] : e[0];
-        vertex = v[0] = not isVisited(v[1]) ? v[1] : v[0];
-    }
-    if (isVisited(vertex)) {
-        solusi.push_back({n, graph[n][3]});
-        solusi.push_back({3, 0});
-    } else {
-        solusi.push_back({n, edge});
-        FindShortest(vertex, 0, 3, edge, vertex);
+        mid = (start + end) / 2;
+        ShortestRoute(rumah, start, mid, e[0], v[0]);
+        ShortestRoute(rumah, mid + 1, end, e[1], v[1]);
+        if (e[0] < e[1]) {
+            if (not isVisited(v[0])) {
+                vertex = v[1] = v[0];
+                edge = e[1] = e[0];
+            } else {
+                vertex = v[0] = v[1];
+                edge = e[0] = e[1];
+            }
+        } else {
+            if (not isVisited(v[1])) {
+                vertex = v[0] = v[1];
+                edge = e[0] = e[1];
+            } else {
+                vertex = v[1] = v[0];
+                edge = e[1] = e[0];
+            }
+        }
+        if (isVisited(vertex)) {
+            Solusi.push_back({rumah, graph[rumah][3]});
+            Solusi.push_back({3, 0});
+        } else {
+            Solusi.push_back({rumah, edge});
+            ShortestRoute(vertex, 0, 3, edge, vertex);
+        }
     }
 }
 
@@ -64,21 +88,16 @@ char Change(int index) {
     return ' ';
 }
 
-void CheckSolusi() {
-    int sum = 0;
-    for (int i = 0; i < solusi.size() - 1; i++) {
-        std::cout << "Kurir pergi dari rumah " << Change(solusi.at(i).first)
-                  << " ke rumah " << Change(solusi.at(i + 1).first)
-                  << " dengan jarak " << solusi.at(i).second << "m\n";
-        sum += solusi.at(i).second;
-    }
-    std::cout << "Total perjalanan kurir : " << sum << "m"
-              << std::endl;
-}
-
 int main() {
-    static int edge, vertex, sum;
-    FindShortest(3, 0, 3, edge, vertex);
-    CheckSolusi();
+    static int edge, vertex;
+    ShortestRoute(3, 0, 3, edge, vertex);
+    int sum = Solusi.at(1).second;
+    for (int i = 0; i < Solusi.size() - 1; i++) {
+        cout << "Kurir pergi dari rumah " << Change(Solusi.at(i).first)
+                  << " ke rumah " << Change(Solusi.at(i + 1).first)
+                  << " dengan jarak " << Solusi.at(i).second << "m" << endl;
+        sum += Solusi.at(i + 1).second;
+    }
+    cout << "Total perjalanan kurir : " << sum << "m" << endl;
     return 0;
 }
